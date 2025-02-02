6c73a509fcd532d6ab625955b699461adac9a373
Rajnish Kumar Mishra <40133554+rajnishkmishra@users.noreply.github.com>
2025-02-03 02:03:00 +0530

    Update common.go

--- models/vm/common.go
+++ models/vm/common.go
@@ -16,6 +16,20 @@ func NewPaginationRequest(p int64, l int64) PaginationRequest {
 	}
 }
 
+func (p PaginationRequest) GetPageNumber() int64 {
+	if p.PageNumber <= 0 {
+		return 1
+	}
+	return p.PageNumber
+}
+
+func (p PaginationRequest) GetLimit() int64 {
+	if p.Limit <= 0 {
+		return 10
+	}
+	return p.Limit
+}
+
 type MetaResponse struct {
 	TotalPages  int64 `json:"total_pages"`
 	TotalRecord int64 `json:"total_records"`
